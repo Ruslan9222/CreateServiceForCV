eacfd80616146d0d7b0854d33643c4e718354853
Ruslan9222 <magog21@gmail.com>
2024-06-16 12:29:57 +0300

    Initial commit

---
+++ src/main/java/by/ruslan/createserviceforcv/CreateServiceForCvApplication.java
@@ -0,0 +1,13 @@
+package by.ruslan.createserviceforcv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateServiceForCvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateServiceForCvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/ruslan/createserviceforcv/CreateServiceForCvApplicationTests.java
@@ -0,0 +1,13 @@
+package by.ruslan.createserviceforcv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateServiceForCvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
