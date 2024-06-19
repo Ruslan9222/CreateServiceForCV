6f70eca1b7d4fa7d1adfdf07fd2ab6bcea122605
Ruslan9222 <magog21@gmail.com>
2024-06-19 21:26:39 +0300

    fix

--- src/main/java/by/ruslan/createserviceforcv/dto/CreateTestDto.java
+++ src/main/java/by/ruslan/createserviceforcv/dto/CreateTestDto.java
@@ -1,12 +1,13 @@
 package by.ruslan.createserviceforcv.dto;
 
 import by.ruslan.createserviceforcv.model.Direction;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+
+import javax.persistence.CascadeType;
+import javax.persistence.OneToMany;
 import java.util.List;
 
 @Setter
--- src/main/java/by/ruslan/createserviceforcv/model/ResultTests.java
+++ src/main/java/by/ruslan/createserviceforcv/model/ResultTests.java
@@ -1,8 +1,8 @@
 package by.ruslan.createserviceforcv.model;
 
-import jakarta.persistence.*;
 import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
